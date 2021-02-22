080597b0b28cd4bfd0f9cc09635e47a17d259451
Adelson Guimarães Monteiro <adelsonguimaraes@gmail.com>
2021-02-22 14:52:49 -0400

    limpando

--- homeCtrl.js
+++
@@ -1,7 +0,0 @@
-angular.module(module).controller('homeCtrl', function ($rootScope, $scope, authenticationAPI, genericAPI, $location, SweetAlert, $uibModal, $timeout) {
-    //Verifica Sessao e permissão de acesso
-    if (!$rootScope.usuario) { $location.path("/login"); return false; }
-
-    $scope.title = 'Home';
-    
-});
